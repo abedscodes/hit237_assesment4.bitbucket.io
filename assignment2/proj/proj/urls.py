56ec48e5f683469e66715660a4d692ce5204c714
abedscodes <s361222@students.cdu.edu.au>
2024-04-16 21:47:04 +0930

    moved home to proj_app

--- assignment2/proj/proj/urls.py
+++ assignment2/proj/proj/urls.py
@@ -18,9 +18,9 @@ from django.contrib import admin
 from django.urls import path
 from proj_app import views
 
-urlpatterns = [
-    path('', views.home, name='home'),
+urlpatterns = [    
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     
 ]
