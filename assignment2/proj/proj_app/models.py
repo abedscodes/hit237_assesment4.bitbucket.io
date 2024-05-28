22e0d931adffe9fd379584f9fa33f50d7a1cf193
abedscodes <s361222@students.cdu.edu.au>
2024-05-29 00:17:07 +0930

    edited models

--- assignment2/proj/proj_app/models.py
+++ assignment2/proj/proj_app/models.py
@@ -11,31 +11,33 @@ class Supervisor(models.Model):
     def __str__(self):
         return str(self.staffID) + " : " + self.name
 
-class Group(models.Model):
-    groupID = models.CharField(max_length=6, primary_key=True)
-    name = models.CharField(max_length=100)
-    supervisors = models.ManyToManyField(Supervisor)
-    is_approved = models.BooleanField(default=False)  # Field to track if the group's thesis project is approved
 
-    def __str__(self):
-        return self.name
-         
+        
 
 class Topic(models.Model):
     topicID = models.CharField(max_length=4, primary_key=True)
     title = models.CharField(max_length=200)
     supervisor = models.ForeignKey(Supervisor, on_delete=models.CASCADE)
-    groups = models.ManyToManyField(Group, related_name='theses')
     is_approved = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
 
-class Student(models.Model):
-    studentID = models.CharField(max_length=6, primary_key=True)
+class Group(models.Model):
+    groupID = models.CharField(max_length=6, primary_key=True)
     name = models.CharField(max_length=100)
+    supervisors = models.ManyToManyField(Supervisor)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    is_approved = models.BooleanField(default=False)  # Field to track if the group's thesis project is approved
     email = models.EmailField()
-    group = models.ForeignKey(Group, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
+    
+class Application(models.Model):
+    applicationID = models.CharField(max_length=6, primary_key=True)
+    groupID = models.ForeignKey(Group, on_delete=models.CASCADE)
+    topicID = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.applicationID
